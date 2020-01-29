09b88cbf93af516d0a5abbcecb132ba0e069a6d3
harishbabu07 <60419328+harishbabu07@users.noreply.github.com>
2020-01-29 12:47:45 +0530

    Create uber.py

---
+++ uber.py
@@ -0,0 +1,42 @@
+print("welcome to uber destination available central,purasaivakam,avadi")
+print("\ncentral,\npurasaivakam,\nkarayanchavadi,\navadi")
+d=raw_input("Enter your destination:")
+if(d=="avd"):
+    print("source:karayanchavadi\n")
+    print("destination:avd\n")
+    print("total fare:60")
+    print("total km:15")
+elif (d=="cmbt"):
+    print("source:avd\n")
+    print("destination:cmbt\n")
+    print("total fare:95")
+    print("total km:20")
+elif(d=="central"):
+    print("source:purasivakam\n")
+    print("destination:central\n")
+    print("total fare:100")
+    print("total km:21")
+        
+
+'''
+input:avd
+output:
+welcome to uber destination available central,purasaivakam,avadi
+
+central,
+purasaivakam,
+karayanchavadi,
+avadi
+Enter your destination:avd
+source:karayanchavadi
+
+destination:avd
+
+total fare:60
+total km:15
+
+
+
+
+
+'''
